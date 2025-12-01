39d070d558301bffb91e9f39ca2a0a07046047bd
abhayswami2004-source <abhayswami2004@gmail.com>
2025-12-01 18:04:42 +0530

    Create Home component for Simple CRM dashboard

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>Welcome to Simple CRM</h2>
+      <p>This is your dashboard. Use the navigation to manage contacts.</p>
+    </div>
+  );
+}
